5dfa18b89c1e71f560c1522984a7eba6346c4d19
Vimal-Yappes <vimal.raj@yappes.com>
2022-02-23 20:00:54 +0530

    Setting Up Basic Test init with addition.test.js

---
+++ test/addition.test.js
@@ -0,0 +1,5 @@
+describe('Testing Addition',()=>{
+    test.todo("Empty String");
+    test.todo("Simple String with one value");
+    test.todo("Simple String with two value and comma as seperator")
+})
